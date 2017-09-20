ad8965a821719ddd3b5ffd6013714bcd7890f151
Catie Stallings <catiestallings@Caties-MacBook-Pro.local>
2017-09-20 14:14:49 -0500

    Catch on error event with error callback

--- s3-zip.js
+++ s3-zip.js
@@ -4,8 +4,9 @@ var archiver = require('archiver')
 var s3Zip = {}
 module.exports = s3Zip
 
-s3Zip.archive = function (opts, folder, filesS3, filesZip) {
+s3Zip.archive = function (opts, folder, filesS3, errorCallback) {
   var self = this
+
   var keyStream = s3Files
     .connect({
       region: opts.region,
@@ -14,32 +15,27 @@ s3Zip.archive = function (opts, folder, filesS3, filesZip) {
     .createKeyStream(folder, filesS3)
 
   var fileStream = s3Files.createFileStream(keyStream)
-  var archive = self.archiveStream(fileStream, filesS3, filesZip)
+  var archive = self.archiveStream(fileStream, filesS3, errorCallback)
   return archive
 }
 
-s3Zip.archiveStream = function (stream, filesS3, filesZip) {
+s3Zip.archiveStream = function (stream, filesS3, errorCallback) {
   var archive = archiver('zip')
   archive.on('error', function (err) {
     console.log('archive error', err)
     throw err
   })
+
   stream
    .on('data', function (file) {
-      // console.log(file.data.toString());
      if (file.path[file.path.length - 1] === '/') {
        console.log('don\'t append to zip', file.path)
        return
      }
      var fname
-     if (filesZip) {
-       // Place files_s3[i] into the archive as files_zip[i]
-       var i = filesS3.indexOf(file.path)
-       fname = (i >= 0 && i < filesZip.length) ? filesZip[i] : file.path
-     } else {
-       const randomNum = Math.random();
-       fname = `${randomNum}_${file.path}`
-     }
+     const randomNum = Math.random();
+     fname = `${randomNum}_${file.path}`
+
      console.log('append to zip', fname)
      if (file.data.length === 0) {
        archive.append('', { name: fname })
@@ -50,6 +46,9 @@ s3Zip.archiveStream = function (stream, filesS3, filesZip) {
    .on('end', function () {
      console.log('end -> finalize')
      archive.finalize()
+   })
+   .on('error', function (err) {
+     errorCallback(err.message)
    })
 
   return archive
